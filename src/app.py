be15f65aa1d4ccecb3982b5cd7fe5c13bba9a2e5
Goran Golijanin <goran.golijanin1@gmail.com>
2025-08-27 12:13:42 -0400

    Uses self-hosted runners

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great! :) '
+        'message': 'You are doing great!! :)'
     })
 
 @app.route('/api/v1/healthz')
