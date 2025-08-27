492e4af8f344100bec315924bdd6a1b9b809d736
Goran Golijanin <goran.golijanin1@gmail.com>
2025-08-27 11:13:46 -0400

    update config files

--- src/app.py
+++ src/app.py
@@ -3,19 +3,21 @@ import datetime, socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details', methods=['GET'])
-def details():
-	return jsonify(
-		{ 
-			# "time": datetime.datetime.now().strftime("%H:%M:%S %Y-%m-%d"),
-			"time": datetime.datetime.now().strftime("%Y-%m-%d"),
-			"hostname": socket.gethostname(),
-			"message": "You are doing great!"
-		}), 200
+@app.route('/api/v1/info')
 
-@app.route('/api/v1/health', methods=['GET'])
+def info():
+    return jsonify({
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3'
+    })
+
+@app.route('/api/v1/healthz')
+
 def health():
-	return jsonify({ "status": "up" }), 200
+	# Do an actual check here
+    return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True)
+
+    app.run(host="0.0.0.0")
